bfa78fc7755f2d2c281775123d804b3859e8152c
abbyrjones72 <abbyrjones72@gmail.com>
2020-12-29 13:17:45 -0500

    adding navbar and social icons

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Home() {
+    return (
+        <main>
+            <img alt="this is an welcome page" />
+            <section>
+               <h1>Hello, I am Abby!</h1>  
+            </section>   
+        </main>
+    )
+}
